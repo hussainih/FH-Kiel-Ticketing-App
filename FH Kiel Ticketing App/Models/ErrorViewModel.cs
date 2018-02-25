7566b18801c6d1bd344e62aab7b3cd95b850f5ac
Hussainih <hussaini.mohd86@gmail.com>
2018-02-25 20:50:39 +0100

    Add project files.

---
+++ FH Kiel Ticketing App/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace FH_Kiel_Ticketing_App.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
